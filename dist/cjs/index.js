44b4642cda4ebd094bb3623398f8d32bcac3b61a
Alvin John Tongco <tongcoeijei@gmail.com>
2024-07-04 15:35:34 +0800

    Fixed error in importing

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,14 +23,14 @@ var GrantType;
 (function (GrantType) {
     GrantType["PASSWORD"] = "password";
 })(GrantType || (GrantType = {}));
-const init = async (connectionOptions) => {
+const init = (connectionOptions) => __awaiter(void 0, void 0, void 0, function* () {
     let responseTypes = [];
     let grantType = null;
     if (connectionOptions.flow === SupportedFlow.DIRECT_GRANT) {
         responseTypes = ['id_token'];
         grantType = GrantType.PASSWORD;
     }
-    const issuer = await openid_client_1.Issuer.discover(connectionOptions.basePath);
+    const issuer = yield openid_client_1.Issuer.discover(connectionOptions.basePath);
     const client = new issuer.Client({
         client_id: connectionOptions.clientId,
         client_secret: connectionOptions.clientSecret,
@@ -33,71 +42,79 @@ const init = async (connectionOptions) => {
         grantType,
         clientId: connectionOptions.clientId,
     };
-};
+});
 exports.init = init;
 class OidcService {
     constructor(oidcConnect) {
         this.oidcConnect = oidcConnect;
         this.oidcConnect = oidcConnect;
     }
-    async logout(accessToken, idToken, sessionState) {
-        try {
-            const url = await this.oidcConnect.client.endSessionUrl({
-                id_token_hint: idToken,
-                state: sessionState,
-                client_id: this.oidcConnect.clientId,
-            });
-            await this.oidcConnect.client.revoke(accessToken);
-            await (0, node_fetch_1.default)(url);
-        }
-        catch (e) {
-            throw new Error('Error in logging out');
-        }
-    }
-    async refreshToken(refreshToken) {
-        try {
-            const newTokenSet = await this.oidcConnect.client.refresh(refreshToken);
-            return {
-                accessToken: newTokenSet.access_token,
-                refreshToken: newTokenSet.refresh_token,
-                expiresIn: newTokenSet.expires_in,
-            };
-        }
-        catch (e) {
-            throw new Error('Error in refreshing token');
-        }
-    }
-    async introspect(accessToken) {
-        try {
-            const introspectionResponse = await this.oidcConnect.client.introspect(accessToken);
-            if (!introspectionResponse || (introspectionResponse && !introspectionResponse.active)) {
-                return false;
+    logout(accessToken, idToken, sessionState) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const url = yield this.oidcConnect.client.endSessionUrl({
+                    id_token_hint: idToken,
+                    state: sessionState,
+                    client_id: this.oidcConnect.clientId,
+                });
+                yield this.oidcConnect.client.revoke(accessToken);
+                yield (0, node_fetch_1.default)(url);
             }
-            return true;
-        }
-        catch (e) {
-            throw new Error('Error in token introspection');
-        }
+            catch (e) {
+                throw new Error('Error in logging out');
+            }
+        });
     }
-    async login(username, password) {
-        try {
-            const tokenSet = await this.oidcConnect.client.grant({
-                grant_type: this.oidcConnect.grantType || GrantType.PASSWORD,
-                username,
-                password,
-                scope: 'openid profile email'
-            });
-            return {
-                accessToken: tokenSet.access_token,
-                refreshToken: tokenSet.refresh_token,
-                expiresIn: tokenSet.expires_in,
-                sessionState: tokenSet.session_state,
-                idToken: tokenSet.id_token,
-            };
-        }
-        catch (e) {
-            throw new Error('Error in logging in');
-        }
+    refreshToken(refreshToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const newTokenSet = yield this.oidcConnect.client.refresh(refreshToken);
+                return {
+                    accessToken: newTokenSet.access_token,
+                    refreshToken: newTokenSet.refresh_token,
+                    expiresIn: newTokenSet.expires_in,
+                };
+            }
+            catch (e) {
+                throw new Error('Error in refreshing token');
+            }
+        });
+    }
+    introspect(accessToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const introspectionResponse = yield this.oidcConnect.client.introspect(accessToken);
+                if (!introspectionResponse || (introspectionResponse && !introspectionResponse.active)) {
+                    return false;
+                }
+                return true;
+            }
+            catch (e) {
+                throw new Error('Error in token introspection');
+            }
+        });
+    }
+    login(username, password) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const tokenSet = yield this.oidcConnect.client.grant({
+                    grant_type: this.oidcConnect.grantType || GrantType.PASSWORD,
+                    username,
+                    password,
+                    scope: 'openid profile email'
+                });
+                return {
+                    accessToken: tokenSet.access_token,
+                    refreshToken: tokenSet.refresh_token,
+                    expiresIn: tokenSet.expires_in,
+                    sessionState: tokenSet.session_state,
+                    idToken: tokenSet.id_token,
+                };
+            }
+            catch (e) {
+                throw new Error('Error in logging in');
+            }
+        });
     }
 }
 exports.OidcService = OidcService;
